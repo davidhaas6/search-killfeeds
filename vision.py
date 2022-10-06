bf20a4ff574b423dd0b54c01ad534a099a824301
David Haas <davidhaas@Davids-MacBook-Air.local>
2022-10-05 20:43:26 -0400

    moved notebooks

--- vision.py
+++ vision.py
@@ -2,12 +2,28 @@ from matplotlib import pyplot as plt
 import cv2 
 import numpy as np
 import pytesseract
+import os
+import tqdm
 
 def preprocess(img,invert=False):
     img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
     img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     if invert: img = cv2.bitwise_not(img)
     return img
+
+
+
+def batch_preprocess(paths, out_dir=None, invert=False):
+    imgs = [preprocess(cv2.imread(p)) for p in paths]
+
+    if out_dir is not None:
+        if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+        for i, im in tqdm.tqdm(enumerate(imgs)):
+            file_path = os.path.join(out_dir, f'{i}.png')
+            cv2.imwrite(file_path,im)
+
+    return imgs
 
 
 def crop_topright_quadrant(img):
