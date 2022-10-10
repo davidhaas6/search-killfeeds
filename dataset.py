ddb26ed65d4634682d5c9918a60ec65e069c7f38
David Haas <davidhaas6@gmail.com>
2022-10-10 00:49:03 -0500

    added cropping

--- dataset.py
+++ dataset.py
@@ -40,7 +40,7 @@ def preprocess_frame(frame):
     return topright_crop(frame,0.4,0.4)
 
 
-def process_video(video_id: str, fs: float, res="1080p"):
+def process_video(video_id: str, fs: float, res="1080p", show=False):
     logging.info(f"Pulling {id_to_url(video_id)}")
     clock = TimeArr()
     im_buff, stream = pull_video(video_id, "mp4", res)
@@ -54,14 +54,23 @@ def process_video(video_id: str, fs: float, res="1080p"):
     det = NNTextDetect('weights/east_text_detection_weights.pb')
 
     clock.report(logging.info)
+
+    extracted_kf_rows = []
     for img in tqdm(cropped, total=len(frames)):
         txt_boxes = det.detect_text(img)
-        det.draw_bboxes(img, txt_boxes, True)
         if len(txt_boxes) == 0:
             continue
-
+        if show:
+            det.draw_bboxes(img, txt_boxes, True)
         combo_boxes = combine_boxes(txt_boxes)
-        det.draw_bboxes(img, combo_boxes, True)
+        if show:
+            det.draw_bboxes(img, combo_boxes, True)
+        for bbox in combo_boxes:
+            x0,y0,x1,y1 = bbox
+            kf_row = img[y0:y1,x0:x1,:]
+            extracted_kf_rows.append(kf_row)
+
+    return extracted_kf_rows
         # cv2.imshow(combo_boxes)
 
 
@@ -69,14 +78,17 @@ def process_video(video_id: str, fs: float, res="1080p"):
 def main():
     logging.basicConfig(level = logging.INFO)
 
-    vid_id = '2hu1bUHL1mc'
-    if len(argv) >= 2:
-        if argv[1] == "stabby": 
-            vid_id = "TYkkWeOzlbM"
-        elif argv[1] == "scream":
-            vid_id = "H9d2c1oevUU"
+    vids = {
+        "stabby": "TYkkWeOzlbM",
+        "scream": "H9d2c1oevUU",
+        "eggop": "-BGMI34rAl8",
+        "18s": '2hu1bUHL1mc',
+    }
+    vid_id = vids['eggop']
+    if len(argv) > 1:
+        vid_id = vids[argv[1]] if argv[1] in vids else argv[1]
 
-    process_video(vid_id, 1)
+    process_video(vid_id, 1, "1080p", True)
 
 
 if __name__ == "__main__":
